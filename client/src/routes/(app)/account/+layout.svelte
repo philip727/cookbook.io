18bc66fa2bdfd856da8f2e2cac18f304322e526f
philip727 <philster36@hotmail.co.uk>
2024-06-07 16:09:06 +0100

    account page start

---
+++ client/src/routes/(app)/account/+layout.svelte
@@ -0,0 +1,9 @@
+<section class="flex w-full h-fit justify-center items-center">
+    <div class="shadow-one flex flex-row items-start justify-start w-4/5 mt-20 p-4 bg-white">
+        <div class="w-40 flex flex-col items-start justify-start text-xs gap-1">
+            <h1>Public Profile</h1>
+            <h1>Security</h1>
+        </div>
+        <slot />
+    </div>
+</section>
