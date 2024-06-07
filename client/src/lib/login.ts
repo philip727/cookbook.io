0b8b08212334c93471e0d64dba768c9e89df5d5e
philip727 <philster36@hotmail.co.uk>
2024-06-07 16:08:25 +0100

    login is now verified on initial website load

--- client/src/lib/login.ts
+++ client/src/lib/login.ts
@@ -1,8 +1,38 @@
 import { writable } from 'svelte/store';
+import { endpoint } from './api';
 export const JWT_TOKEN_KEY = "jwt_authorization_token";
 
 export type User = {
     uid: number,
     username: string,
 }
+
 export const user = writable<User | null>(null);
+
+export const requestJWTVerification = async (key: string): Promise<User | null> => {
+    try {
+        let bearer = "Bearer " + key;
+        console.log(bearer);
+        let response = await fetch(
+            endpoint("/account/verify") as string,
+            {
+                headers: {
+                    Authorization: bearer,
+                },
+            },
+        );
+
+        if (!response.ok) {
+            let text = await response.text();
+            console.log(text);
+            return null;
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+
+        return null
+    }
+}
--- client/src/routes/+layout.ts
+++ client/src/routes/+layout.ts
@@ -1,1 +1,20 @@
 export const ssr = false;
+
+import { JWT_TOKEN_KEY, requestJWTVerification, user } from "$lib/login";
+
+
+export const load = async () => {
+    let key = window.localStorage[JWT_TOKEN_KEY];
+    if (key == null) {
+        return;
+    }
+
+    let jwtClaims = await requestJWTVerification(key);
+    if (jwtClaims == null) {
+        return;
+    }
+
+    user.set({
+        ...jwtClaims
+    });
+}
