56a04cf0d84d4850711203ce0fb81137e97f2595
philip727 <philster36@hotmail.co.uk>
2024-06-09 13:29:23 +0100

    client multiline

---
+++ client/src/components/TextMultilineInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let name: string | undefined = undefined;
+    export let placeholder = "...";
+    export let extraClass = "text";
+    export let value: any = undefined;
+</script>
+
+<textarea
+    class={extraClass +
+        " " +
+        "border-2 border-black w-full px-2 py-2 focus:border-[var(--green)] focus:outline-none duration-150 transition-colors font-normal resize-none h-32 pt-1"}
+    {placeholder}
+    {name}
+    bind:value
+/>
