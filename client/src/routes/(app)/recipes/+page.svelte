13dfaf9e1158951d445bd3c29b180e59a8d8fc61
philip727 <philster36@hotmail.co.uk>
2024-06-15 17:09:29 +0100

    recipe display

--- client/src/routes/(app)/recipes/+page.svelte
+++ client/src/routes/(app)/recipes/+page.svelte
@@ -11,23 +11,25 @@
         <p>Failed to load recipes</p>
     </div>
 {:else if data.recipes}
-    <div class="flex flex-row flex-wrap gap-2 items-center justify-center">
+    <div class="flex flex-row flex-wrap gap-2 items-center justify-center py-4">
         {#each data.recipes as post}
             <a href={`/recipes/${post.id}`}>
-                <div class="w-80 h-72">
+                <div class="w-80 h-fit bg-gray-100 border border-[#00000000] hover:border-[var(--yellow)] box-border shadow-black duration-150 transition-all shadow-hover">
                     {#if post.thumbnail != null}{/if}
                     <img
                         class="w-full h-48"
                         src={endpoint(`/thumbnails/${post.thumbnail}`)}
                     />
-                    <div>
-                        <h1 class="text-xl font-bold">{post.title}</h1>
-                        <p class="text-sm text-gray-700">
-                            {post.description}
-                        </p>
-                    </div>
-                    <div>
-                        <UserPreview user={post.poster} />
+                    <div class="pb-3 px-3 pt-2">
+                        <div>
+                            <h1 class="text-xl font-bold">{post.title}</h1>
+                            <p class="text-sm text-gray-700">
+                                {post.description}
+                            </p>
+                        </div>
+                        <div class="mt-4 flex flex-row justify-end">
+                            <UserPreview user={post.poster} />
+                        </div>
                     </div>
                 </div>
             </a>
